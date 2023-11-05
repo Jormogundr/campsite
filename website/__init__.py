47018269c9a8227eb760b1265c0df23b5ba75087
Nate Pierce <nate.pierce@maymobility.com>
2023-11-05 14:01:06 -0500

    more views, including profile

--- website/__init__.py
+++ website/__init__.py
@@ -4,14 +4,14 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "campsite.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     # local testing, use app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////home/nate/{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////home/nate/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
