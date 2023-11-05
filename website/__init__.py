be095e8e8abb636702ac6ba36c254b06cda86428
Nate Pierce <nate.pierce@maymobility.com>
2023-11-05 14:30:19 -0500

    reflect db before trying to create

--- website/__init__.py
+++ website/__init__.py
@@ -20,10 +20,10 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User
     
     with app.app_context():
-        db.create_all()
+        db.reflect()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
