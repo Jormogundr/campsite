46ac724c972a359e609a2c6ff44889429227d18c
Nate Pierce <nate.pierce@maymobility.com>
2023-11-25 23:50:37 -0500

    set SQLalchemy engine options to make heroku happy

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 # load env vars
 load_dotenv()
 
-#database 
+# database
 db = SQLAlchemy()
 db_name = os.getenv("DB_NAME")
 db_url = os.getenv("DATABASE_URL")
@@ -21,32 +21,43 @@ max_file_sz = os.getenv("MAX_CONENT_LENGTH")
 secret_key = os.getenv("SECRET_KEY")
 
 # for local testing
-DB_NAME = "test30.db"
+# DB_NAME = "test30.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = secret_key
-    app.config['MAX_CONENT_LENGTH'] = max_file_sz
+    app.config["SECRET_KEY"] = secret_key
+    app.config["MAX_CONENT_LENGTH"] = max_file_sz
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": dict(
+            dbname=db_name,
+            sslcert="client.crt",
+            sslkey="client.key",
+            sslrootcert="ca.crt",
+            sslmode="verify-full",
+            ssl_min_protocol_version="TLSv1.3",
+        )
+    }
 
     # production db
-    # app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////home/nate/{DB_NAME}'
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = SQLALCHEMY_ENGINE_OPTIONS
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User, CampSite
-    
+
     with app.app_context():
         db.create_all()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -57,6 +68,6 @@ def create_app():
 
 
 def create_database(app):
-    if not os.path.exists('website/' + db_name):
+    if not os.path.exists("website/" + db_name):
         db.create_all(app=app)
-        print('Created Database!')
+        print("Created Database!")
