50545de5270696259e9d636c1fb52ad5334a02b9
Nate Pierce <nate.pierce@maymoblity.com>
2024-11-10 13:01:41 -0500

    Email notification/observer working

--- website/config.py
+++ website/config.py
@@ -20,10 +20,10 @@ class Config:
     }
 
     # Email SMTP
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_TLS=True,
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
     MAIL_USERNAME=os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD=os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER=('Campsite App', 'your-email@gmail.com'),
-    SITE_URL='http://localhost:5000'        # Move to environment variable os.getenv('SITE_URL')
+    MAIL_DEFAULT_SENDER=('Campsite App', 'nathancp93@gmail.com')
+    SITE_URL=os.getenv('SITE_URL')
